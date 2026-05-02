9e73b5626327283bf384102f242399afa6e37dd1
Miguel Rodríguez <jazmindemar33@gmail.com>
2026-05-02 16:52:36 +0200

    Start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortafolioNavbar } from "./navbar/portafolioNavbar/portafolioNavbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PortafolioNavbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/navbar/sectionList/sectionList.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'section-list',
+  imports: [],
+  templateUrl: './sectionList.html',
+  styleUrl: './sectionList.scss',
+})
+export class SectionList { }
